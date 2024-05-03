1083e3226cfb3a81104d61ad5c794a1b4789bf51
turavas <olapola971@gmail.com>
2024-05-03 12:12:40 +0200

    HW_2.2.1/Fixed calculations.js

--- calculations.js
+++ calculations.js
@@ -3,16 +3,17 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
+function decreaseBalance(x, y) {
+x = x - y; 
 return x;
-}
+} 
+//добавлен аргумент у, т.к потом мы используем в функцмм переменную у, которая нигде не объявлена 
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
-}
+} /*console.log("x"); после return не воспринимается компилятором. 
+Позже мы выводим результат в консоль при вызове функции с предачей значений аргументов */
 
 function getRestAfterDivision(x, y) {
   x = x % y;
@@ -21,5 +22,7 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2)); 
+// убрала 4000, т.к. в этой функции предаем только два аргумента
+
 console.log(getRestAfterDivision(7000, 3));
